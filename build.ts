cd29a0180a85172ec53bdd4d3821767e4d6e12a7
Roman <nabukhotnyiroman@gmail.com>
2024-08-22 17:50:31 +0300

    feat: Add prisma sqlite

--- build.ts
+++ build.ts
@@ -1,44 +1,56 @@
-import * as esbuild from "esbuild";
+import * as esbuild from 'esbuild';
 
 esbuild.buildSync({
-  entryPoints: ["./src/drizzle-server-node.ts"],
+  entryPoints: ['./src/drizzle-server-node.ts'],
   bundle: true,
   minify: true,
-  outfile: "dist/drizzle.js",
-  format: "cjs",
-  target: "node22",
-  platform: "node",
-  external: [
-    "esbuild",
-    "pg-native",
-  ],
+  outfile: 'dist/drizzle.js',
+  format: 'cjs',
+  target: 'node22',
+  platform: 'node',
+  external: ['esbuild', 'pg-native'],
 });
 
 esbuild.buildSync({
-  entryPoints: ["./src/prisma-server-node.ts"],
+  entryPoints: ['./src/prisma-server-node.ts'],
   bundle: true,
   minify: true,
-  outfile: "dist/prisma.js",
-  format: "cjs",
-  target: "node22",
-  platform: "node",
-  external: [
-    "esbuild",
-    "pg-native",
-  ],
+  outfile: 'dist/prisma.js',
+  format: 'cjs',
+  target: 'node22',
+  platform: 'node',
+  external: ['esbuild', 'pg-native'],
 });
 
-
 esbuild.buildSync({
-  entryPoints: ["./src/prisma-joins-server-node.ts"],
+  entryPoints: ['./src/prisma-joins-server-node.ts'],
   bundle: true,
   minify: true,
-  outfile: "dist/prisma-joins.js",
-  format: "cjs",
-  target: "node22",
-  platform: "node",
-  external: [
-    "esbuild",
-    "pg-native",
-  ],
+  outfile: 'dist/prisma-joins.js',
+  format: 'cjs',
+  target: 'node22',
+  platform: 'node',
+  external: ['esbuild', 'pg-native'],
+});
+
+esbuild.buildSync({
+  entryPoints: ['./src/sqlite/drizzle-server-node.ts'],
+  bundle: true,
+  minify: true,
+  outfile: 'dist/sqlite/drizzle.js',
+  format: 'cjs',
+  target: 'node22',
+  platform: 'node',
+  external: ['esbuild', 'better-sqlite3'],
+});
+
+esbuild.buildSync({
+  entryPoints: ['./src/sqlite/prisma-server-node.ts'],
+  bundle: true,
+  minify: true,
+  outfile: 'dist/sqlite/prisma.js',
+  format: 'cjs',
+  target: 'node22',
+  platform: 'node',
+  external: ['esbuild', 'better-sqlite3'],
 });
---
+++ src/sqlite/prisma-server-bun.ts
@@ -0,0 +1,211 @@
+
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+import cpuUsage from "../cpu-usage";
+import os from "os"
+
+const prisma = new PrismaClient();
+
+const app = new Hono();
+app.route('', cpuUsage);
+app.get("/customers", async (c) => {
+  const limit = Number(c.req.query("limit"));
+  const offset = Number(c.req.query("offset"));
+
+  const result = await prisma.customer.findMany({
+    take: limit,
+    skip: offset,
+  });
+
+  return c.json(result);
+});
+
+app.get("/customer-by-id", async (c) => {
+  const result = await prisma.customer.findFirst({
+    where: {
+      id: Number(c.req.query("id")!),
+    },
+  });
+  return c.json(result);
+});
+
+// app.get("/search-customer", async (c) => {
+//   const result = await prisma.customer.findMany({
+//     where: {
+//       companyName: {
+//         search: `${c.req.query("term")}:*`,
+//       },
+//     },
+//   });
+
+//   return c.json(result);
+// });
+
+app.get("/employees", async (c) => {
+  const limit = Number(c.req.query("limit"));
+  const offset = Number(c.req.query("offset"));
+
+  const result = await prisma.employee.findMany({
+    take: limit,
+    skip: offset,
+  });
+  return c.json(result);
+});
+
+app.get("/employee-with-recipient", async (c) => {
+  const result = await prisma.employee.findUnique({
+    where: {
+      id: Number(c.req.query("id")!),
+    },
+    include: {
+      recipient: true,
+    },
+  });
+  return c.json([result]);
+});
+
+app.get("/suppliers", async (c) => {
+  const limit = Number(c.req.query("limit"));
+  const offset = Number(c.req.query("offset"));
+
+  const result = await prisma.supplier.findMany({
+    take: limit,
+    skip: offset,
+  });
+  return c.json(result);
+});
+
+app.get("/supplier-by-id", async (c) => {
+  const result = await prisma.supplier.findUnique({
+    where: {
+      id: Number(c.req.query("id")!),
+    },
+  });
+  return c.json(result);
+});
+
+app.get("/products", async (c) => {
+  const limit = Number(c.req.query("limit"));
+  const offset = Number(c.req.query("offset"));
+
+  const result = await prisma.product.findMany({
+    take: limit,
+    skip: offset,
+  });
+  return c.json(result);
+});
+
+app.get("/product-with-supplier", async (c) => {
+  const result = await prisma.product.findUnique({
+    where: {
+      id: Number(c.req.query("id")!),
+    },
+    include: {
+      supplier: true,
+    },
+  });
+  return c.json([result]);
+});
+
+// app.get("/search-product", async (c) => {
+//   const result = await prisma.product.findMany({
+//     where: {
+//       name: {
+//         search: `${c.req.query("term")}:*`,
+//       },
+//     },
+//   });
+
+//   return c.json(result);
+// });
+
+app.get("/orders-with-details", async (c) => {
+  const limit = Number(c.req.query("limit"));
+  const offset = Number(c.req.query("offset"));
+
+  const res = await prisma.order.findMany({
+    include: {
+      details: true,
+    },
+    take: limit,
+    skip: offset,
+    orderBy: {
+      id: "asc",
+    },
+  });
+
+  const result = res.map((item) => {
+    return {
+      id: item.id,
+      shippedDate: item.shippedDate,
+      shipName: item.shipName,
+      shipCity: item.shipCity,
+      shipCountry: item.shipCountry,
+      productsCount: item.details.length,
+      quantitySum: item.details.reduce(
+        (sum, deteil) => (sum += +deteil.quantity),
+        0
+      ),
+      totalPrice: item.details.reduce(
+        (sum, deteil) => (sum += +deteil.quantity * +deteil.unitPrice),
+        0
+      ),
+    };
+  });
+  return c.json(result);
+});
+
+app.get("/order-with-details", async (c) => {
+  const res = await prisma.order.findMany({
+    include: {
+      details: true,
+    },
+    where: {
+      id: Number(c.req.query("id")!),
+    },
+  });
+
+  const result = res.map((item) => {
+    return {
+      id: item.id,
+      shippedDate: item.shippedDate,
+      shipName: item.shipName,
+      shipCity: item.shipCity,
+      shipCountry: item.shipCountry,
+      productsCount: item.details.length,
+      quantitySum: item.details.reduce(
+        (sum, detail) => (sum += detail.quantity),
+        0
+      ),
+      totalPrice: item.details.reduce(
+        (sum, detail) => (sum += detail.quantity * detail.unitPrice.toNumber()),
+        0
+      ),
+    };
+  });
+
+  return c.json(result);
+});
+
+app.get("/order-with-details-and-products", async (c) => {
+  const result = await prisma.order.findMany({
+    where: {
+      id: Number(c.req.query("id")!),
+    },
+    include: {
+      details: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return c.json(result);
+});
+
+export default {
+  fetch: app.fetch,
+  port: 3001,
+  reusePort: true,
+}
