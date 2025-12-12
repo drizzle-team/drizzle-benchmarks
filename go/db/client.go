0d781328cc48b4536a0235c8c345e72b3397c8a6
thelovekesh <lovekesh.kumar@rtcamp.com>
2025-12-12 21:28:32 +0530

    Set max conn

---
+++ go/db/client.go
@@ -0,0 +1,68 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/bytedance/sonic"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Client struct {
+	*Queries
+	pool *pgxpool.Pool
+}
+
+func NewDatabase(databaseUrl string) (*Client, error) {
+	config, err := pgxpool.ParseConfig(databaseUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	config.MaxConns = 200
+	config.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+		sonicCodec := &pgtype.JSONCodec{
+			Marshal:   sonic.Marshal,
+			Unmarshal: sonic.Unmarshal,
+		}
+
+		tm := conn.TypeMap()
+
+		tm.RegisterType(&pgtype.Type{
+			Name:  "json",
+			OID:   pgtype.JSONOID,
+			Codec: sonicCodec,
+		})
+
+		tm.RegisterType(&pgtype.Type{
+			Name:  "jsonb",
+			OID:   pgtype.JSONBOID,
+			Codec: sonicCodec,
+		})
+
+		return nil
+	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = pool.Ping(ctx); err != nil {
+		pool.Close()
+
+		return nil, fmt.Errorf("unable to ping database: %w", err)
+	}
+
+	return &Client{pool: pool, Queries: New(pool)}, nil
+}
+
+func (db *Client) Close() {
+	db.pool.Close()
+}
