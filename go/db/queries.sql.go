96c306c056c7c6553e8e799c9f55e4dcaf4571e2
thelovekesh <lovekesh.kumar@rtcamp.com>
2025-12-12 21:28:57 +0530

    Fix search queries

--- go/db/queries.sql.go
+++ go/db/queries.sql.go
@@ -12,16 +12,11 @@ import (
 )
 
 const customerById = `-- name: CustomerById :one
-select "d0"."id" as "id", "d0"."company_name" as "companyName", "d0"."contact_name" as "contactName", "d0"."contact_title" as "contactTitle", "d0"."address" as "address", "d0"."city" as "city", "d0"."postal_code" as "postalCode", "d0"."region" as "region", "d0"."country" as "country", "d0"."phone" as "phone", "d0"."fax" as "fax" from "customers" as "d0" where "d0"."id" = $1 limit $2
+select "d0"."id" as "id", "d0"."company_name" as "companyName", "d0"."contact_name" as "contactName", "d0"."contact_title" as "contactTitle", "d0"."address" as "address", "d0"."city" as "city", "d0"."postal_code" as "postalCode", "d0"."region" as "region", "d0"."country" as "country", "d0"."phone" as "phone", "d0"."fax" as "fax" from "customers" as "d0" where "d0"."id" = $1
 `
 
-type CustomerByIdParams struct {
-	ID    int32
-	Limit int32
-}
-
-func (q *Queries) CustomerById(ctx context.Context, arg CustomerByIdParams) (Customer, error) {
-	row := q.db.QueryRow(ctx, customerById, arg.ID, arg.Limit)
+func (q *Queries) CustomerById(ctx context.Context, id int32) (Customer, error) {
+	row := q.db.QueryRow(ctx, customerById, id)
 	var i Customer
 	err := row.Scan(
 		&i.ID,
@@ -81,13 +76,8 @@ func (q *Queries) Customers(ctx context.Context, arg CustomersParams) ([]Custome
 }
 
 const employeeWithRecipient = `-- name: EmployeeWithRecipient :one
-select "d0"."id" as "id", "d0"."last_name" as "lastName", "d0"."first_name" as "firstName", "d0"."title" as "title", "d0"."title_of_courtesy" as "titleOfCourtesy", "d0"."birth_date" as "birthDate", "d0"."hire_date" as "hireDate", "d0"."address" as "address", "d0"."city" as "city", "d0"."postal_code" as "postalCode", "d0"."country" as "country", "d0"."home_phone" as "homePhone", "d0"."extension" as "extension", "d0"."notes" as "notes", "d0"."recipient_id" as "recipientId", "recipient"."r" as "recipient" from "employees" as "d0" left join lateral(select row_to_json("t".*) "r" from (select "d1"."id" as "id", "d1"."last_name" as "lastName", "d1"."first_name" as "firstName", "d1"."title" as "title", "d1"."title_of_courtesy" as "titleOfCourtesy", "d1"."birth_date" as "birthDate", "d1"."hire_date" as "hireDate", "d1"."address" as "address", "d1"."city" as "city", "d1"."postal_code" as "postalCode", "d1"."country" as "country", "d1"."home_phone" as "homePhone", "d1"."extension" as "extension", "d1"."notes" as "notes", "d1"."recipient_id" as "recipientId" from "employees" as "d1" where "d0"."recipient_id" = "d1"."id" limit $1) as "t") as "recipient" on true where "d0"."id" = $2
+select "d0"."id" as "id", "d0"."last_name" as "lastName", "d0"."first_name" as "firstName", "d0"."title" as "title", "d0"."title_of_courtesy" as "titleOfCourtesy", "d0"."birth_date" as "birthDate", "d0"."hire_date" as "hireDate", "d0"."address" as "address", "d0"."city" as "city", "d0"."postal_code" as "postalCode", "d0"."country" as "country", "d0"."home_phone" as "homePhone", "d0"."extension" as "extension", "d0"."notes" as "notes", "d0"."recipient_id" as "recipientId", "recipient"."r" as "recipient" from "employees" as "d0" left join lateral(select row_to_json("t".*) "r" from (select "d1"."id" as "id", "d1"."last_name" as "lastName", "d1"."first_name" as "firstName", "d1"."title" as "title", "d1"."title_of_courtesy" as "titleOfCourtesy", "d1"."birth_date" as "birthDate", "d1"."hire_date" as "hireDate", "d1"."address" as "address", "d1"."city" as "city", "d1"."postal_code" as "postalCode", "d1"."country" as "country", "d1"."home_phone" as "homePhone", "d1"."extension" as "extension", "d1"."notes" as "notes", "d1"."recipient_id" as "recipientId" from "employees" as "d1" where "d0"."recipient_id" = "d1"."id") as "t") as "recipient" on true where "d0"."id" = $1
 `
-
-type EmployeeWithRecipientParams struct {
-	Limit int32
-	ID    int32
-}
 
 type EmployeeWithRecipientRow struct {
 	ID              int32
@@ -108,8 +98,8 @@ type EmployeeWithRecipientRow struct {
 	Recipient       []byte
 }
 
-func (q *Queries) EmployeeWithRecipient(ctx context.Context, arg EmployeeWithRecipientParams) (EmployeeWithRecipientRow, error) {
-	row := q.db.QueryRow(ctx, employeeWithRecipient, arg.Limit, arg.ID)
+func (q *Queries) EmployeeWithRecipient(ctx context.Context, id int32) (EmployeeWithRecipientRow, error) {
+	row := q.db.QueryRow(ctx, employeeWithRecipient, id)
 	var i EmployeeWithRecipientRow
 	err := row.Scan(
 		&i.ID,
@@ -227,13 +217,8 @@ func (q *Queries) OrderWithDetails(ctx context.Context, id int32) (OrderWithDeta
 }
 
 const orderWithDetailsAndProducts = `-- name: OrderWithDetailsAndProducts :one
-select "d0"."id" as "id", "d0"."order_date" as "orderDate", "d0"."required_date" as "requiredDate", "d0"."shipped_date" as "shippedDate", "d0"."ship_via" as "shipVia", "d0"."freight" as "freight", "d0"."ship_name" as "shipName", "d0"."ship_city" as "shipCity", "d0"."ship_region" as "shipRegion", "d0"."ship_postal_code" as "shipPostalCode", "d0"."ship_country" as "shipCountry", "d0"."customer_id" as "customerId", "d0"."employee_id" as "employeeId", "details"."r" as "details" from "orders" as "d0" left join lateral(select coalesce(json_agg(row_to_json("t".*)), '[]') as "r" from (select "d1"."unit_price" as "unitPrice", "d1"."quantity" as "quantity", "d1"."discount" as "discount", "d1"."order_id" as "orderId", "d1"."product_id" as "productId", "product"."r" as "product" from "order_details" as "d1" left join lateral(select row_to_json("t".*) "r" from (select "d2"."id" as "id", "d2"."name" as "name", "d2"."qt_per_unit" as "quantityPerUnit", "d2"."unit_price" as "unitPrice", "d2"."units_in_stock" as "unitsInStock", "d2"."units_on_order" as "unitsOnOrder", "d2"."reorder_level" as "reorderLevel", "d2"."discontinued" as "discontinued", "d2"."supplier_id" as "supplierId" from "products" as "d2" where "d1"."product_id" = "d2"."id" limit $1) as "t") as "product" on true where "d0"."id" = "d1"."order_id") as "t") as "details" on true where "d0"."id" = $2
+select "d0"."id" as "id", "d0"."order_date" as "orderDate", "d0"."required_date" as "requiredDate", "d0"."shipped_date" as "shippedDate", "d0"."ship_via" as "shipVia", "d0"."freight" as "freight", "d0"."ship_name" as "shipName", "d0"."ship_city" as "shipCity", "d0"."ship_region" as "shipRegion", "d0"."ship_postal_code" as "shipPostalCode", "d0"."ship_country" as "shipCountry", "d0"."customer_id" as "customerId", "d0"."employee_id" as "employeeId", "details"."r" as "details" from "orders" as "d0" left join lateral(select coalesce(json_agg(row_to_json("t".*)), '[]') as "r" from (select "d1"."unit_price" as "unitPrice", "d1"."quantity" as "quantity", "d1"."discount" as "discount", "d1"."order_id" as "orderId", "d1"."product_id" as "productId", "product"."r" as "product" from "order_details" as "d1" left join lateral(select row_to_json("t".*) "r" from (select "d2"."id" as "id", "d2"."name" as "name", "d2"."qt_per_unit" as "quantityPerUnit", "d2"."unit_price" as "unitPrice", "d2"."units_in_stock" as "unitsInStock", "d2"."units_on_order" as "unitsOnOrder", "d2"."reorder_level" as "reorderLevel", "d2"."discontinued" as "discontinued", "d2"."supplier_id" as "supplierId" from "products" as "d2" where "d1"."product_id" = "d2"."id") as "t") as "product" on true where "d0"."id" = "d1"."order_id") as "t") as "details" on true where "d0"."id" = $1
 `
-
-type OrderWithDetailsAndProductsParams struct {
-	Limit int32
-	ID    int32
-}
 
 type OrderWithDetailsAndProductsRow struct {
 	ID             int32
@@ -252,8 +237,8 @@ type OrderWithDetailsAndProductsRow struct {
 	Details        interface{}
 }
 
-func (q *Queries) OrderWithDetailsAndProducts(ctx context.Context, arg OrderWithDetailsAndProductsParams) (OrderWithDetailsAndProductsRow, error) {
-	row := q.db.QueryRow(ctx, orderWithDetailsAndProducts, arg.Limit, arg.ID)
+func (q *Queries) OrderWithDetailsAndProducts(ctx context.Context, id int32) (OrderWithDetailsAndProductsRow, error) {
+	row := q.db.QueryRow(ctx, orderWithDetailsAndProducts, id)
 	var i OrderWithDetailsAndProductsRow
 	err := row.Scan(
 		&i.ID,
@@ -324,13 +309,8 @@ func (q *Queries) OrdersWithDetails(ctx context.Context, arg OrdersWithDetailsPa
 }
 
 const productWithSupplier = `-- name: ProductWithSupplier :one
-select "d0"."id" as "id", "d0"."name" as "name", "d0"."qt_per_unit" as "quantityPerUnit", "d0"."unit_price" as "unitPrice", "d0"."units_in_stock" as "unitsInStock", "d0"."units_on_order" as "unitsOnOrder", "d0"."reorder_level" as "reorderLevel", "d0"."discontinued" as "discontinued", "d0"."supplier_id" as "supplierId", "supplier"."r" as "supplier" from "products" as "d0" left join lateral(select row_to_json("t".*) "r" from (select "d1"."id" as "id", "d1"."company_name" as "companyName", "d1"."contact_name" as "contactName", "d1"."contact_title" as "contactTitle", "d1"."address" as "address", "d1"."city" as "city", "d1"."region" as "region", "d1"."postal_code" as "postalCode", "d1"."country" as "country", "d1"."phone" as "phone" from "suppliers" as "d1" where "d0"."supplier_id" = "d1"."id" limit $1) as "t") as "supplier" on true where "d0"."id" = $2
+select "d0"."id" as "id", "d0"."name" as "name", "d0"."qt_per_unit" as "quantityPerUnit", "d0"."unit_price" as "unitPrice", "d0"."units_in_stock" as "unitsInStock", "d0"."units_on_order" as "unitsOnOrder", "d0"."reorder_level" as "reorderLevel", "d0"."discontinued" as "discontinued", "d0"."supplier_id" as "supplierId", "supplier"."r" as "supplier" from "products" as "d0" left join lateral(select row_to_json("t".*) "r" from (select "d1"."id" as "id", "d1"."company_name" as "companyName", "d1"."contact_name" as "contactName", "d1"."contact_title" as "contactTitle", "d1"."address" as "address", "d1"."city" as "city", "d1"."region" as "region", "d1"."postal_code" as "postalCode", "d1"."country" as "country", "d1"."phone" as "phone" from "suppliers" as "d1" where "d0"."supplier_id" = "d1"."id") as "t") as "supplier" on true where "d0"."id" = $1
 `
-
-type ProductWithSupplierParams struct {
-	Limit int32
-	ID    int32
-}
 
 type ProductWithSupplierRow struct {
 	ID              int32
@@ -345,8 +325,8 @@ type ProductWithSupplierRow struct {
 	Supplier        []byte
 }
 
-func (q *Queries) ProductWithSupplier(ctx context.Context, arg ProductWithSupplierParams) (ProductWithSupplierRow, error) {
-	row := q.db.QueryRow(ctx, productWithSupplier, arg.Limit, arg.ID)
+func (q *Queries) ProductWithSupplier(ctx context.Context, id int32) (ProductWithSupplierRow, error) {
+	row := q.db.QueryRow(ctx, productWithSupplier, id)
 	var i ProductWithSupplierRow
 	err := row.Scan(
 		&i.ID,
@@ -415,7 +395,7 @@ func (q *Queries) Products(ctx context.Context, arg ProductsParams) ([]ProductsR
 }
 
 const searchCustomer = `-- name: SearchCustomer :many
-select "d0"."id" as "id", "d0"."company_name" as "companyName", "d0"."contact_name" as "contactName", "d0"."contact_title" as "contactTitle", "d0"."address" as "address", "d0"."city" as "city", "d0"."postal_code" as "postalCode", "d0"."region" as "region", "d0"."country" as "country", "d0"."phone" as "phone", "d0"."fax" as "fax" from "customers" as "d0" where to_tsvector('english', "customers"."company_name") @@ to_tsquery('english', $1)
+select "d0"."id" as "id", "d0"."company_name" as "companyName", "d0"."contact_name" as "contactName", "d0"."contact_title" as "contactTitle", "d0"."address" as "address", "d0"."city" as "city", "d0"."postal_code" as "postalCode", "d0"."region" as "region", "d0"."country" as "country", "d0"."phone" as "phone", "d0"."fax" as "fax" from "customers" as "d0" where to_tsvector('english', "d0"."company_name") @@ to_tsquery('english', $1)
 `
 
 func (q *Queries) SearchCustomer(ctx context.Context, toTsquery string) ([]Customer, error) {
@@ -451,7 +431,7 @@ func (q *Queries) SearchCustomer(ctx context.Context, toTsquery string) ([]Custo
 }
 
 const searchProduct = `-- name: SearchProduct :many
-select "d0"."id" as "id", "d0"."name" as "name", "d0"."qt_per_unit" as "quantityPerUnit", "d0"."unit_price" as "unitPrice", "d0"."units_in_stock" as "unitsInStock", "d0"."units_on_order" as "unitsOnOrder", "d0"."reorder_level" as "reorderLevel", "d0"."discontinued" as "discontinued", "d0"."supplier_id" as "supplierId" from "products" as "d0" where to_tsvector('english', "products"."name") @@ to_tsquery('english', $1)
+select "d0"."id" as "id", "d0"."name" as "name", "d0"."qt_per_unit" as "quantityPerUnit", "d0"."unit_price" as "unitPrice", "d0"."units_in_stock" as "unitsInStock", "d0"."units_on_order" as "unitsOnOrder", "d0"."reorder_level" as "reorderLevel", "d0"."discontinued" as "discontinued", "d0"."supplier_id" as "supplierId" from "products" as "d0" where to_tsvector('english', "d0"."name") @@ to_tsquery('english', $1)
 `
 
 type SearchProductRow struct {
@@ -497,16 +477,11 @@ func (q *Queries) SearchProduct(ctx context.Context, toTsquery string) ([]Search
 }
 
 const supplierById = `-- name: SupplierById :one
-select "d0"."id" as "id", "d0"."company_name" as "companyName", "d0"."contact_name" as "contactName", "d0"."contact_title" as "contactTitle", "d0"."address" as "address", "d0"."city" as "city", "d0"."region" as "region", "d0"."postal_code" as "postalCode", "d0"."country" as "country", "d0"."phone" as "phone" from "suppliers" as "d0" where "d0"."id" = $1 limit $2
+select "d0"."id" as "id", "d0"."company_name" as "companyName", "d0"."contact_name" as "contactName", "d0"."contact_title" as "contactTitle", "d0"."address" as "address", "d0"."city" as "city", "d0"."region" as "region", "d0"."postal_code" as "postalCode", "d0"."country" as "country", "d0"."phone" as "phone" from "suppliers" as "d0" where "d0"."id" = $1
 `
 
-type SupplierByIdParams struct {
-	ID    int32
-	Limit int32
-}
-
-func (q *Queries) SupplierById(ctx context.Context, arg SupplierByIdParams) (Supplier, error) {
-	row := q.db.QueryRow(ctx, supplierById, arg.ID, arg.Limit)
+func (q *Queries) SupplierById(ctx context.Context, id int32) (Supplier, error) {
+	row := q.db.QueryRow(ctx, supplierById, id)
 	var i Supplier
 	err := row.Scan(
 		&i.ID,
